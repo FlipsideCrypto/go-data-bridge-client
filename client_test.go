b678dbe4db01970870df40a42516ec3a8e584429
Don Cote <mistercote@gmail.com>
2019-09-16 13:34:53 -0400

    remove consumer from config

--- client_test.go
+++ client_test.go
@@ -22,7 +22,7 @@ func TestClient_GetUnreadCount(t *testing.T) {
 
 func TestClient_GetNextRecord(t *testing.T) {
 	client := getClient(t)
-	r, err := client.GetNextRecord()
+	r, err := client.GetNextRecord("b053b974-608e-4f1b-9969-871a02cfbf92")
 	if err != nil {
 		t.Fatalf("Unexpected error: %v", err)
 	}
@@ -69,7 +69,7 @@ func TestClient_RegisterConsumer(t *testing.T) {
 }
 
 func getClient(t *testing.T) Client {
-	config := Config{APIKey: "15d4fbbc-d12e-4b08-a4d6-b55b92200649", TopicSlug: "dev-alert-fcas-events", ConsumerID: "84d3cba6-8be8-4848-b759-59093f2e74e6"}
+	config := Config{APIKey: "15d4fbbc-d12e-4b08-a4d6-b55b92200649", TopicSlug: "dev-alert-fcas-events"}
 	client, err := NewClient(config)
 	if err != nil {
 		t.Fatalf("Unexpected error: %v", err)
